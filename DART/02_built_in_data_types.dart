d92cc97104e4af928ab96682dbfb18d19ef18b1f
Jatin Agarwal <jatinagarwal825@gmail.com>
2026-02-25 11:09:47 +0530

    DART

--- DART/02_built_in_data_types.dart
+++ DART/02_built_in_data_types.dart
@@ -1,27 +1,35 @@
 
 void main(List<String> arguments) {
+  // Numbers: int
+  int score = 23;
+  var count = 23;
+  int hexValue = 0xEADEBAEE;
 
-	// Numbers: int
-	int score = 23;
-	var count = 23;     
-	int hexValue = 0xEADEBAEE;
+  // Numbers: double
+  double percentage = 93.4;
+  var percent = 82.533;
+  double exponents = 1.42e5;
 
-	// Numbers: double
-	double percentage = 93.4;
-	var percent = 82.533;
-	double exponents = 1.42e5; 
+  // Strings
+  String name = "Henry";
+  var company = "Google";
 
-	// Strings
-	String name = "Henry";
-	var company = "Google";
+  // Boolean
+  bool isValid = true;
+  var isAlive = false;
 
-	// Boolean
-	bool isValid = true;
-	var isAlive = false;
+  // Print all values
+  print(score);
+  print(count);
+  print(hexValue);
+  print(percentage);
+  print(percent);
+  print(exponents);
+  print(name);
+  print(company);
+  print(isValid);
+  print(isAlive);
 
-	print(score);
-	print(exponents);
-
-	// NOTE: All data types in Dart are Objects.
-	// Therefore, their initial value is by default 'null'
+  // NOTE: All data types in Dart are Objects.
+  // In Dart with null safety, non-nullable types must be initialized
 }
