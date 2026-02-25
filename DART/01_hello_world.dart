33f8e2ea87cba606ab2ae2e9c5a8dd78c1ce3ca3
Jatin Agarwal <jatinagarwal825@gmail.com>
2026-02-25 11:31:25 +0530

    Commit

--- DART/01_hello_world.dart
+++
@@ -1,7 +0,0 @@
-void main(){
-  print("Hello World");
-
-  print("This is my first application");
-  print(12/4);
-  print(false);
-}
--- DART/nn.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:io';
-
-void main() {
-  String a = stdin.readLineSync() ?? "Guest";
-  print(a);
-}
